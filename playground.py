7142743b9ec72d0de3118e50a3783247b6157e20
Eckybrazzz <mike.e@vecks.com.br>
2022-01-26 15:48:58 -0300

    whle loop

--- playground.py
+++ playground.py
@@ -179,6 +179,27 @@ while x < 5:
 #   if n == 1:
 #     return True
 #   return False
+
+
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'numCells' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY grid as parameter.
+#
+  
   
 
 # print(is_power_of_two(0)) # Should be False
@@ -204,3 +225,59 @@ def jane():
 jane()
 fred()
 jane()
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'numCells' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY grid as parameter.
+#
+
+def numCells(grid):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grid_rows = int(input().strip())
+    grid_columns = int(input().strip())
+
+    grid = []
+
+    for _ in range(grid_rows):
+        grid.append(list(map(int, input().rstrip().split())))
+
+    result = numCells(grid)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+n = 0
+while True:
+    if n == 3:
+        break
+    print(n)
+    n = n + 1
