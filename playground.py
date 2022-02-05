2942441dc4527de0d8bfe202842b08c7a6e583ab
Eckybrazzz <mike.e@vecks.com.br>
2022-02-05 19:22:35 -0300

    add classes

--- playground.py
+++ playground.py
@@ -302,3 +302,36 @@ a = [1, 2, 3]
 b = [4, 5, 6]
 c = a + b
 print(c)
+
+#
+# Example file for working with classes
+#
+
+
+class myClass():
+    def method1(self):
+        print("myClass method1")
+
+    def method2(self, someString):
+        print("myClass method2: " + someString)
+
+
+class anotherClass(myClass):
+    def method2(self):
+        print("anotherClass method2")
+
+    def method1(self):
+        myClass.method1(self)
+        print("anotherClass method1")
+
+
+def main():
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
+    c2 = anotherClass()
+    c2.method1()
+
+
+if __name__ == "__main__":
+    main()
